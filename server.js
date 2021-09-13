687bd9d69f914fb55c4bbd3f781de1219e173c2c
Myuze <Myuze@users.noreply.github.com>
2021-09-12 19:33:16 -0700

    Added nodemon server script

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 
+app = express();
 app.use(express.json());
+
+// App Routes
 
 app.listen(PORT, () => {
   console.log(`Running at http://localhost:${PORT}`)
