a6394036fb90bcbb2043916d4376c1beecf9d90b
Myuze <Myuze@users.noreply.github.com>
2021-09-12 21:25:47 -0700

    Added variables for local credentials

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const mysql = require('mysql2');
 const { nanoid } = require('nanoid');
 const express = require('express');
 
+// Local DB Credentials
+const pass = require('./password');
+
+// Environment Variables
 const PORT = process.env.PORT || 3001;
+const PASS = process.env.DB_PASS || pass;
 
 app = express();
 app.use(express.json());
@@ -12,13 +17,34 @@ const db = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: PASS,
     database: 'cms_db'
   },
   console.log(`Connected to the cms_db database`)
 );
 
 // App Routes
+app.get('/departments', (req, res) => {
+  db.query('SELECT * FROM department', (err, result) => {
+    console.log(result);
+    res.json(result);
+  });
+});
+
+app.get('/roles', (req, res) => {
+  db.query('SELECT * FROM role', (err, result) => {
+    console.log(result);
+    res.json(result);
+  });
+});
+
+app.get('employees', (req, res) => {
+  db.query('SELECT * FROM employee', (err, result) => {
+    console.log(result);
+    res.json(result);
+  });
+});
+
 app.get('*', (req, res) => {
   db.query('SELECT * FROM employee', (err, result) => {
     console.log(result);
